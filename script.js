ae530deb559e56966c9c3260404af140aef7b12b
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-26 19:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn=document.querySelector("#sum_btn");
+btn.addEventListener("click",()=>{
+	let num1=document.querySelector("#n1").value;
+	let num2=document.querySelector("#n2").value;
+	let output=document.querySelector("#sum");
+	output.innerText=parseInt(num1)+parseInt(num2);
+	
+});
